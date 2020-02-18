8cbb7dd732e3b35c837edd59e0c61fe87d4d28ff
junichi sunaga <1224577blue@gmail.com>
2020-02-18 14:41:23 +0000

    タイムゾーンを日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
+
+    #timezoneを日本時間に設定
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
