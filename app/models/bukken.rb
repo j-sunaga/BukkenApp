4171b2031095e00ec5e80704195d90d3fde98670
junichi sunaga <1224577blue@gmail.com>
2020-02-28 14:55:10 +0000

    物件情報の各項目に空文字が登録できないようにバリデーションを設定

--- app/models/bukken.rb
+++ app/models/bukken.rb
@@ -2,6 +2,14 @@ class Bukken < ApplicationRecord
   #アソシエーション
   has_many :stations
 
+  #バリデーション
+   validates :name, presence: true
+   validates :price, presence: true
+   validates :address, presence: true
+   validates :year, presence: true
+   validates :note, presence: true
+   
+
   #子要素も登録する
   accepts_nested_attributes_for :stations,allow_destroy: true
 
