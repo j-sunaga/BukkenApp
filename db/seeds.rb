5495f4b70401477eef9109b42d94ff3e10143c27
junichi sunaga <1224577blue@gmail.com>
2020-03-01 04:00:40 +0000

    初期データ用のCSVファイルの作成とインポートするコードをseed.rbに記載

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+#csvファイルを扱うためのgemを読み込む
+
+CSV.foreach('db/bukken.csv') do |row|
+#foreachは、ファイル（hoge.csv）の各行を引数として、ブロック(do~endまでを範囲とする『引数のかたまり』)を繰り返し実行する
+#rowには、読み込まれた行が代入される
+Bukken.create(:name=>row[0],:price=>row[1],:address=>row[2],:year=>row[3],:note=>row[4],stations_attributes:[:line=>row[5],:station_name=>row[6],:walk_time=>row[7]])
+
+end
